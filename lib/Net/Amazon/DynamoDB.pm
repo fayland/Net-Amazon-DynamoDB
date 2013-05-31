caa19d3fd5a5276a524882e0dc8b7c418fcb2ff8
Fayland Lam <fayland@gmail.com>
2013-05-31 19:56:20 +0800

    Fix moo also

--- lib/Net/Amazon/DynamoDB.pm
+++ lib/Net/Amazon/DynamoDB.pm
@@ -1448,7 +1448,7 @@ sub batch_get_item {
                 $cache_key{$table}{$key_ref->{$hash_key}} = $cache_key;
                 my $cached = $self->cache->thaw( $cache_key );
                 if (defined $cached) {
-                    $cache_res{ $table_out } = [];
+                    $cache_res{ $table_out } ||= [];
                     push @{ $cache_res{ $table_out } }, $cached;
                     next;
                 }
